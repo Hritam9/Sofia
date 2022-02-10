f541e6016ee9ed1386bb8bf6122568b6258d8ae2
Hritam Jaiwal <43675990+Hritam9@users.noreply.github.com>
2022-02-10 19:16:12 +0530

    Update move.py

--- move.py
+++ move.py
@@ -3,8 +3,8 @@ import os
 import glob
 import time
 #source = os.listdir("D://Video SONGS/Videos/")
-source = "D://Video SONGS/Videos/*.mkv"
-destination = "D://Video SONGS/"
+source = "YOUR FILE LOCATION"
+destination = "FILE LOCATION"
 file_list = glob.glob(source)
 for single_file in file_list:
     shutil.move(single_file,destination)
